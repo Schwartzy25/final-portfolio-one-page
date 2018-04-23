82b3b7280e1dbdae5c7fcac5be92a7babcf58740
Schwartzy25 <rebeccaschwartz@rebeccas-mbp.mynet>
2018-04-23 23:40:42 +0100

    presentation version

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $(function() {
 	    }, 1000);
 	});
 
+	$('#hamburger').on('click', function() {
+		$('nav').slideToggle();
+	});
+
+	$(window).on('resize', function() {
+		if ($(window).width() >= 600 && !$('nav').is(':visible')) {
+			$('nav').show();
+		} else if ($(window).width() < 600 && $('nav').is(':visible')) {
+			$('nav').hide();
+		}
+	});
+
 });
